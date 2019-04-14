68fb31feb6366474440d252433091b3f0baa0e7b
saud <saud@192.168.1.2>
2019-04-14 16:59:35 +0300

    cleared everything

--- src/main/resources/checkKeyTool.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Java\jdk1.8.0_144\bin\keytool.exe" --list -keystore mykey.keystore
-pause
--- src/main/resources/genKeyTool.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Java\jdk1.8.0_144\bin\keytool.exe" -genkey -keyalg RSA -alias alias -keystore mykey.keystore -storepass 123456 -keypass 123456
-pause
