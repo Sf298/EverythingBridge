5d6e5424647c5d1be75ec97a930d34c98a91a506
saud <saud@LAPTOP-SLM08CGF>
2019-11-11 19:51:48 +0000

    bug fixes and made buttons work

--- src/main/java/com/sacide/everythingbridge/Main.java
+++ src/main/java/com/sacide/everythingbridge/Main.java
@@ -1,103 +1,43 @@
 package com.sacide.everythingbridge;
 
 import com.sacide.smart.home.api.compilation.DevicesManager;
-import com.sacide.smart.home.api.compilation.LifxCommanderW;
-import com.sacide.smart.home.api.compilation.backend.Device;
-import com.sacide.smart.home.api.compilation.backend.LightDevice;
+import com.sf298.genericwebserver.*;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Map;
+import sauds.toolbox.encryption.Encryptor;
 
 public class Main {
     
     public static final String JAR_NAME = "PC Controller.jar";
     public static final String VERSION = "V0.1";
-    private static UserManagerV um;
-    private static ParamEditorV pe;
+    private static DefaultUserManager um;
     private static DevicesManager dm;
     
-    public static void main(String[] progArgs) throws InterruptedException, IOException {
-        pe = new ParamEditorV();
-		pe.init();
-        um = new UserManagerV(true, "psiuofgosurf", "fineowufnoerun");
-		um.addUser("saud", Encryptor.hashSHA256("a"));
-
+    public static void main(String[] args) throws InterruptedException, IOException {
+		String keystoreFilePath = (args.length < 1) ? "D:\\Dropbox\\Java Projects\\EverythingBridge\\src\\main\\resources\\mykey.keystore" : args[0];
+		String storepass = (args.length < 2) ? "123456" : args[1];
+		String keypass = (args.length < 3) ? "123456" : args[2];
+		
+        um = new DefaultUserManager(false, "psiuofgosurf", "fineowufnoerun");
+		um.addUser("saud", Encryptor.hashSHA256("a", ""));
+		um.setPAC(PagesAccessChecker.ALLOWED);
+		
 		dm = new DevicesManager();
-		LifxCommanderW lifx = new LifxCommanderW();
-		for(Device d : lifx.discoverDevices()) {
-			/*System.out.print(d);
-			System.out.print(" ");
-			System.out.println(d.getIp_id());*/
-			dm.addDevice(d);
-		}
+		dm.scanDevices();
 		/*for(int i=10; i<15; i++) {
 			dm.addDevice(lifx.toRGBLightDevice(new Device("192.168.0."+i, 100, "MYLIGHt "+i))); // test devices to test website
 		}*/
-
-			//System.out.println(new File("/users.prop").getAbsolutePath());
-
-		SHTMLServer server = new SHTMLServer(8889,
-			pe.getParamAsString(ParamEditorV.SSL_KEY_FILE),
-			pe.getParamAsString(ParamEditorV.SSL_KEY_STORE_PASS),
-			pe.getParamAsString(ParamEditorV.SSL_KEYPASS)
-		);
-
-
-		WebPageLoader.addContextsToServer(server, um, pe, dm);
-
+		
+		SHTMLServer server = new SHTMLServer(keystoreFilePath, storepass, keypass);
+		WSFilesLoaderInit.addToServer(server, um, "WebPages");
+		WSLoginInit.addToServer(server, "Everythingbridge", "/home.html", um);
+		WebPageLoader.addContextsToServer(server, um, dm);
+		
 		server.start();
     }
-    
-    private static void doAction(Map<String, String> args) {
-        /*switch(Integer.parseInt(args.get("action"))) {
-            case VOLUME_SET:
-                //PCFunctions.volumeSet(Integer.parseInt(args.get("arg1")));
-                System.out.println("VOLUME_SET");
-                break;
-            case VOLUME_CHANGE:
-                //PCFunctions.volumeChange(Integer.parseInt(args.get("arg1")));
-                System.out.println("VOLUME_CHANGE");
-                break;
-
-            case POWER_HIBERNATE:
-                //PCFunctions.computerHibernate();
-                System.out.println("POWER_HIBERNATE");
-                break;
-
-            case NETFLIX_SPACE:
-                //PCFunctions.sendKey(KeyEvent.VK_SPACE);
-                System.out.println("NETFLIX_SPACE");
-                break;
-            case NETFLIX_FWD:
-                //PCFunctions.sendKey(KeyEvent.VK_RIGHT);
-                System.out.println("NETFLIX_FWD");
-                break;
-            case NETFLIX_BCK:
-                //PCFunctions.sendKey(KeyEvent.VK_LEFT);
-                System.out.println("NETFLIX_BCK");
-                break;
-
-            case MOUSE_MOVEBY:
-                /*PCFunctions.mouseMoveBy(
-                        Double.parseDouble(args.get("arg1")),
-                        Double.parseDouble(args.get("arg2")));*
-                System.out.println("MOUSE_MOVEBY");
-            case MOUSE_MOVETO:
-                /*PCFunctions.mouseMoveTo(
-                        Integer.parseInt(args.get("arg1")),
-                        Integer.parseInt(args.get("arg2")));*
-                System.out.println("MOUSE_MOVETO");
-                break;
-                
-            case MOUSE_CLICK:
-                //PCFunctions.mouseClick(Integer.parseInt(args.get("arg1")));
-                System.out.println("MOUSE_CLICK");
-                break;
-        }*/
-    }
-    
-    public static void printMessage(String line) {
+	
+    public static void logMessage(String line) {
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         System.out.println(sdf.format(cal.getTime()) + "   " + line);
--- src/main/java/com/sacide/everythingbridge/WebPageLoader.java
+++ src/main/java/com/sacide/everythingbridge/WebPageLoader.java
@@ -6,10 +6,11 @@
 package com.sacide.everythingbridge;
 
 import LifxCommander.Messages.DataTypes.HSBK;
-import static com.sacide.everythingbridge.Main.printMessage;
+import static com.sacide.everythingbridge.Main.logMessage;
 import com.sacide.smart.home.api.compilation.DevicesManager;
 import com.sacide.smart.home.api.compilation.backend.*;
-import com.sf298.saudstoolbox.DeepFileIterator;
+import com.sf298.genericwebserver.*;
+import com.sf298.genericwebserver.WSLoginInit;
 import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
@@ -23,103 +24,59 @@ import net.freeutils.httpserver.HTTPServer;
  */
 public class WebPageLoader {
 
-	public static void addContextsToServer(SHTMLServer server, UserManagerV um, ParamEditorV pe, DevicesManager dm) {
-		addStaticContexts(server, um);
-		addServices(server, um);
+	public static void addContextsToServer(SHTMLServer server, UserManager um, DevicesManager dm) {
 		addDynamicPages(server, um, dm);
-		WebPageLoader.addRedirect(server, "/", "/login.html");
+		addServices(server, um, dm);
 	}
-
-	private static void addStaticContexts(SHTMLServer server, UserManagerV um) {
-		File root = new File(WebPageLoader.class.getClassLoader().getResource("WebPages").getFile().replace("%20", " "));
-		DeepFileIterator dfi = new DeepFileIterator(root);
-		while (dfi.hasNext()) {
-			File file = dfi.next();
-			String context = file.toString().substring(root.toString().length()).replace("\\", "/");
-			System.out.println(context);
-			try {
-				if (context.endsWith("login.html") || context.endsWith(".png")
-						|| context.endsWith(".js") || context.endsWith(".css")) {
-					server.addContext(context, new HTTPServer.FileContextHandler(file), "GET");
-				} else {
-					server.addContext(context, (HTTPServer.Request req, HTTPServer.Response resp) -> {
-						Map<String, String> params = req.getParams();
-						int token = Integer.parseInt(params.getOrDefault("token", "-1"));
-						if (token != -1 && um.checkToken(token)) {
-							String page = readWholeFile(file);
-							if (context.endsWith(".html")) {
-								resp.getHeaders().add("Content-Type", "text/html");
-							}
-							resp.send(200, page);
-						} else {
-							resp.sendError(403, "Invalid login");
+	
+	private static void addServices(SHTMLServer server, UserManager um, DevicesManager dm) {
+		server.addContext("/deviceAction", new HTTPServer.ContextHandler() { // sessionID, deviceID, action, params...
+			@Override
+			public int serve(HTTPServer.Request req, HTTPServer.Response resp) throws IOException {
+				if (WSLoginInit.checkSessionIDAndReplyError(req, resp, um.getPAC()))
+					return 0;
+				
+				Map<String, String> params = req.getParams();
+				Device d = dm.getDevice(Integer.parseInt(params.get("deviceID")));
+				
+				String action = params.get("action");
+				String newValue;
+				switch(action) {
+					case "SetPowerState":
+						newValue = params.get("newValue");
+						if(d == null || !(d instanceof OnOffDevice)) {
+							resp.sendError(403);
+							resp.close();
+							return 0;
 						}
-						resp.close();
-						return 0;
-					});
+						((OnOffDevice)d).setPowerState(Boolean.parseBoolean(newValue));
+						break;
+					case "SetBrightness":
+						newValue = params.get("newValue"); // [1-100]
+						if(d == null || !(d instanceof LightDevice)) {
+							resp.sendError(403);
+							resp.close();
+							return 0;
+						}
+						((LightDevice)d).setLightBrightness(Double.parseDouble(newValue), 0);
+						break;
 				}
-			} catch (IOException ex) {
+				
+				resp.send(200, "true");
+				resp.close();
+				return 0;
 			}
-		}
-	}
-
-	private static void addServices(SHTMLServer server, UserManagerV um) {
-		server.addContext("/logout", (HTTPServer.Request req, HTTPServer.Response resp) -> {
-			Map<String, String> params = req.getParams();
-			if (params.containsKey("token")) {
-				try {
-					int token = Integer.parseInt(params.get("token"));
-					um.logout(token);
-				} catch (NumberFormatException ex) {
-				}
-			}
-			resp.send(200, "done");
-			resp.close();
-			return 0;
 		}, "PUT");
-		server.addContext("/logoutUser", (HTTPServer.Request req, HTTPServer.Response resp) -> {
-			Map<String, String> params = req.getParams();
-			if (params.containsKey("token")) {
-				try {
-					int token = Integer.parseInt(params.get("token"));
-					um.logoutUser(token);
-				} catch (NumberFormatException ex) {
-				}
-			}
-			resp.send(200, "done");
-			resp.close();
-			return 0;
-		}, "PUT");
-		server.addContext("/loginChecker", (HTTPServer.Request req, HTTPServer.Response resp) -> { // process token and login credentials
-			Map<String, String> params = req.getParams();
-			if (params.containsKey("token")) {
-				int token = Integer.parseInt(params.get("token"));
-				resp.send(200, String.valueOf(um.checkToken(token)));
-			} else if (params.containsKey("uname") && params.containsKey("pswHash")) {
-				if (um.checkPasswordHash(params.get("uname"), params.get("pswHash"))) {
-					printMessage("User: " + params.get("uname") + " logged in succesfully");
-					resp.send(200, String.valueOf(um.newToken(params.get("uname"))));
-				} else {
-					resp.send(200, "-1");
-				}
-			} else {
-				resp.sendError(403);
-			}
-			resp.close();
-			return 0;
-		}, "GET");
 	}
-
-	private static void addDynamicPages(SHTMLServer server, UserManagerV um, DevicesManager dm) {
+	
+	private static void addDynamicPages(SHTMLServer server, UserManager um, DevicesManager dm) {
 		server.addContext("/home.html", new HTTPServer.ContextHandler() {
 			@Override
 			public int serve(HTTPServer.Request req, HTTPServer.Response resp) throws IOException {
-				Map<String, String> params = req.getParams();
-				String tokenStr = params.get("token");
-				/*if (tokenStr == null || !um.checkToken(Integer.parseInt(tokenStr))) {
+				if (!um.checkSessionID(WSLoginInit.getSessionID(req))) {
 					resp.sendError(403);
 					return 0;
-				}*/
+				}
 
 				StringBuilder contentPartBuilder = new StringBuilder();
 				for (Map.Entry<Integer, Device> entry : dm.getDeviceMap().entrySet()) {
@@ -132,7 +89,7 @@ public class WebPageLoader {
 						deviceImgPath = "/imgs/lightbulb.png";
 					}
 					contentPartBuilder
-							.append("<a href=\"/device.html?token=0twtcht4mtoken0thv303c&deviceid=").append(deviceID).append("\"><div class=\"card\"> \n")
+							.append("<a href=\"/device.html?").append(WSLoginInit.SESSION_ID_URL_PARAM).append("&deviceID=").append(deviceID).append("\"><div class=\"card\"> \n")
 							.append("    <img src=\"").append(deviceImgPath).append("\" alt=\"light bulb img\" width=\"100%\">\n")
 							.append("    <p> ").append(device.getLabel()).append(" </p>\n")
 							.append("</div></a><!--\n")
@@ -188,25 +145,18 @@ public class WebPageLoader {
 						+ "\n"
 						+ "<body class=\"w3-light-grey w3-content\" style=\"max-width:1600px\">\n"
 						+ "    \n"
-						+ "    <script src=\"/tokenCode.js\"></script>\n"
+						+ "    <script src=\"/sessionCode.js\"></script>\n"
 						+ "    \n"
 						+ "    <div class=\"container\">\n"
 						+ "        <h1> Devices </h1>\n"
 						+ "        <div class=\"devicePanel\">\n"
 						+ "            " + contentPart + "\n"
-						+ "            \n"
-						+ "            <!--<a href=\"/custom.html?token=0twtcht4mtoken0thv303c\">Custom Message</a><br>\n"
-						+ "            <a href=\"/netflix.html?token=0twtcht4mtoken0thv303c\">Netflix Remote</a><br>\n"
-						+ "            <a href=\"/mouse.html?token=0twtcht4mtoken0thv303c\">Mouse Control</a><br>\n"
-						+ "            <a href=\"/power.html?token=0twtcht4mtoken0thv303c\">Power Control</a>\n"
-						+ "            <br>\n"
-						+ "            <button onclick=\"JavaScript:logout()\">Logout</button>-->\n"
 						+ "        </div>\n"
 						+ "    </div>\n"
 						+ "\n"
 						+ "\n"
 						+ "    <script type=\"text/javascript\"> \n"
-						+ "        fillPageWithToken();\n"
+						+ "        fillPageWithSessionID();\n"
 						+ "    </script>\n"
 						+ "</body>";
 				// </editor-fold>
@@ -217,13 +167,12 @@ public class WebPageLoader {
 		server.addContext("/device.html", new HTTPServer.ContextHandler() {
 			@Override
 			public int serve(HTTPServer.Request req, HTTPServer.Response resp) throws IOException {
-				Map<String, String> params = req.getParams();
-				String tokenStr = params.get("token");
-				/*if (tokenStr == null || !um.checkToken(Integer.parseInt(tokenStr))) {
+				if (!um.checkSessionID(WSLoginInit.getSessionID(req))) {
 					resp.sendError(403);
 					return 0;
-				}*/
-				Integer deviceID = Integer.parseInt(params.get("deviceid"));
+				}
+				Map<String, String> params = req.getParams();
+				Integer deviceID = Integer.parseInt(params.get("deviceID"));
 				Device device = dm.getDevice(deviceID);
 
 				StringBuilder contentPartBuilder = new StringBuilder();
@@ -232,20 +181,53 @@ public class WebPageLoader {
 					OnOffDevice temp = (OnOffDevice) device;
 					// https://www.w3schools.com/howto/howto_css_switch.asp
 					contentPartBuilder
-							.append("<p style=\"text-align:left;\">Power\n")
-							.append("   <label class=\"switch\" style=\"float:right;\">\n")
-							.append("	    <input name=\"devicePower\" id=\"powerSwitch\" type=\"checkbox\"").append((temp.getPowerState()) ? "checked" : "").append("/>\n")
-							.append("	    <span class=\"slider round\"/>\n")
-							.append("   </label>\n")
-							.append("<p/>");
+							.append("            <script type=\"text/javascript\">\n")
+							.append("                function toggleOnOff() {\n")
+							.append("                    var powerElement = document.getElementById(\"powerSwitch\")\n")
+							.append("                    var newPowerState = powerElement.checked;\n")
+							.append("                    var deviceID = getUrlParameter(\"deviceID\");\n")
+							.append("                    var xhr = new XMLHttpRequest();\n")
+							.append("                    xhr.open('PUT', \"/deviceAction?sessionID=\"+getSessionID()+\"&deviceID=\"+deviceID+\"&action=SetPowerState&newValue=\"+newPowerState, true);\n")
+							.append("                    xhr.send();\n")
+							.append("                    xhr.onreadystatechange = function (e) {\n")
+							.append("                        if (xhr.readyState == 4 && xhr.status == 200) {\n")
+							.append("                            if(xhr.responseText != \"true\") {\n")
+							.append("                                powerElement.checked = !newPowerState;\n")
+							.append("                            }\n")
+							.append("                        }\n")
+							.append("                    };\n")
+							.append("                }\n")
+							.append("            </script>")
+							.append("            <p style=\"text-align:left;\">Power\n")
+							.append("                <label class=\"switch\" style=\"float:right;\">\n")
+							.append("                    <input name=\"devicePower\" id=\"powerSwitch\" onclick=\"toggleOnOff()\" type=\"checkbox\"").append((temp.getPowerState()) ? "checked" : "").append("/>\n")
+							.append("                    <span class=\"slider round\"/>\n")
+							.append("                </label>\n")
+							.append("            <p/>\n");
 				}
 				if (device instanceof LightDevice) {
 					LightDevice temp = (LightDevice) device;
 					// https://www.w3schools.com/howto/howto_js_rangeslider.asp
+					int currVal = (int) (temp.getLightBrightness() * 100);
 					contentPartBuilder
-							.append("<p style=\"text-align:left;\">Brightness\n")
-							.append("	<input name=\"lightBrightness\" id=\"brightnessSlider\" style=\"float:right; width:60%;\" type=\"range\" min=\"1\" max=\"100\" value=\"").append((int) (temp.getLightBrightness() * 100)).append("\" class=\"rangeslider\">\n")
-							.append("<p/>");
+							.append("            <script type=\"text/javascript\">\n")
+							.append("                function updateBrightness() {\n")
+							.append("                    var element = document.getElementById(\"brightnessSlider\")\n")
+							.append("                    var newVal = element.value/100.0;\n")
+							.append("                    var deviceID = getUrlParameter(\"deviceID\");\n")
+							.append("                    var xhr = new XMLHttpRequest();\n")
+							.append("                    xhr.open('PUT', \"/deviceAction?sessionID=\"+getSessionID()+\"&deviceID=\"+deviceID+\"&action=SetBrightness&newValue=\"+newVal, true);\n")
+							.append("                    xhr.send();\n")
+							.append("                    xhr.onreadystatechange = function (e) {\n")
+							.append("                        if (xhr.readyState == 4 && xhr.status == 200) {\n")
+							.append("                            \n")
+							.append("                        }\n")
+							.append("                    };\n")
+							.append("                }\n")
+							.append("            </script>")
+							.append("            <p style=\"text-align:left;\">Brightness\n")
+							.append("                <input name=\"lightBrightness\" id=\"brightnessSlider\" onchange=\"updateBrightness()\" style=\"float:right; width:60%;\" type=\"range\" min=\"0\" max=\"100\" value=\"").append(currVal).append("\" class=\"rangeslider\">\n")
+							.append("            <p/>\n");
 				}
 				if (device instanceof RGBLightDevice) {
 					RGBLightDevice temp = (RGBLightDevice) device;
@@ -265,9 +247,9 @@ public class WebPageLoader {
 
 				String contentPart;
 				if (contentPartBuilder.length() > 0) {
-					contentPart = contentPartBuilder.substring(0, contentPartBuilder.length() - 8);
+					contentPart = contentPartBuilder.toString();
 				} else {
-					contentPart = "Devices not found";
+					contentPart = "Error: device not loaded!";
 				}
 
 				// <editor-fold defaultstate="collapsed" desc=" Website Body ">
@@ -278,7 +260,7 @@ public class WebPageLoader {
 						+ "    <meta charset=\"UTF-8\">\n"
 						+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
 						+ "    \n"
-						+ "    <script src=\"./tokenCode.js\"></script>\n"
+						+ "    <script src=\"./sessionCode.js\"></script>\n"
 						+ "	\n"
 						+ "	<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js\"></script>\n"
 						+ "	<script src=\"jscolor.js\"></script>\n"
@@ -315,12 +297,12 @@ public class WebPageLoader {
 						+ "        <h1> "+device.getLabel()+" </h1>\n"
 						+ "		<br>\n"
 						+ "		<form action=\"show_data.html\">\n"
-						+ "			" + contentPart
+						+ contentPart
 						+ "		</form>\n"
 						+ "    </div>\n"
 						+ "\n"
 						+ "    <script type=\"text/javascript\"> \n"
-						+ "        fillPageWithToken();\n"
+						+ "        fillPageWithSessionID();\n"
 						+ "    </script>\n"
 						+ "</body>";
 				// </editor-fold>
